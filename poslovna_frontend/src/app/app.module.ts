47cd3fa602d4a10ba4fa4a2b1119d8f9f70bd754
Filip <fmarjanovic@tmns.com>
2019-02-07 22:40:51 +0100

    Some additional validation on login page

--- poslovna_frontend/src/app/app.module.ts
+++ poslovna_frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login';
 import {JwtInterceptor} from "./_helpers";
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
@@ -31,7 +32,8 @@ import {HTTP_INTERCEPTORS} from "@angular/common/http";
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS , useClass: JwtInterceptor, multi: true }
