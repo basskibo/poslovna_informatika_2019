25f1455457cb323383a7e93e7f693c2f95623d43
Bojan Jagetic <jagetic.bojan@gmail.com>
2019-02-19 00:47:18 +0100

    backend > currency creating on register, xml is generated for account after acount create

---
+++ poslovna_backend/api/models/Currency.js
@@ -0,0 +1,47 @@
+/**
+ * Currency.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  tableName: 'valuta',
+  primaryKey: 'id',
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    currency_code: {
+      type: 'string',
+      required: true,
+      columnName: 'sifra valute'
+    },
+    currency_name: {
+      type: 'string',
+      required: true,
+      columnName: 'naziv valute'
+    },
+    domicil: {
+      type: 'boolean',
+      columnName: 'domicilna',
+      defaultsTo: true
+    },
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
