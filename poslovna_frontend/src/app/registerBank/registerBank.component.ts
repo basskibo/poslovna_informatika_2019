073efc26d90466eb48d04140b6dab8f3961766fc
Aleksandar Rancic <rancicaca@yahoo.com>
2019-02-22 23:34:37 +0100

    prepravljeni konflikti

--- poslovna_frontend/src/app/registerBank/registerBank.component.ts
+++ poslovna_frontend/src/app/registerBank/registerBank.component.ts
@@ -2,15 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Globals } from '../globals';
-<<<<<<< HEAD
 import { BankService } from '../_services';
 import { first } from 'rxjs/operators';
 import {Router} from '@angular/router';
-=======
-import {Router} from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import {first} from 'rxjs/operators';
->>>>>>> bb6ec7907fbcfe41ee3abae10c064bbd72b4418d
+
 
 @Component({
   selector: 'app-registerBank',
@@ -18,17 +14,32 @@ import {first} from 'rxjs/operators';
   styleUrls: ['./registerBank.component.css']
 })
 export class RegisterBankComponent implements OnInit {
-<<<<<<< HEAD
+  countries;
   registerBankForm: FormGroup;
   loading = false;
   submitted = false;
+  userNotFound = false;
   msg = '';
 
-  constructor( private data: DataService, private globals: Globals,
+  constructor(private http: HttpClient, private data: DataService, private globals: Globals,
     private formBuilder: FormBuilder, private bankService: BankService,
     private router: Router) { }
 
   ngOnInit() {
+    this.http.get('https://restcountries.eu/rest/v2/all')
+    .pipe()
+      .subscribe(
+        response => {
+          this.countries = JSON.stringify(response);
+          console.log(this.countries.name);
+
+        },
+        error => {
+          console.log('Ne Radi' + error);
+          this.loading = false;
+          this.userNotFound = true;
+        });
+
     this.registerBankForm = this.formBuilder.group({
     bankPIB: ['', [Validators.pattern('[0-9]{9}'), Validators.required]],
     bankName: ['', Validators.required],
@@ -80,56 +91,3 @@ export class RegisterBankComponent implements OnInit {
         });
   }
 }
-=======
-  countries;
-  registerNewBankForm: FormGroup;
-  loading = false;
-  submitted = false;
-  userNotFound = false;
-  msg = '';
-
-  constructor(private http: HttpClient, private data: DataService, private formBuilder: FormBuilder,
-              private globals: Globals, private router: Router) {
-  }
-
-  ngOnInit() {
-
-    this.http.get('https://restcountries.eu/rest/v2/all')
-    .pipe()
-      .subscribe(
-        response => {
-          this.countries = JSON.stringify(response);
-          console.log(this.countries.name);
-
-        },
-        error => {
-          console.log('Ne Radi' + error);
-          this.loading = false;
-          this.userNotFound = true;
-        });
-
-    this.registerNewBankForm = this.formBuilder.group({
-      pib: ['', [Validators.required, Validators.pattern('[0-9]{9}')]],
-      bankName: ['', [Validators.required]],
-      bankAddress: ['', [Validators.required]],
-      state: [''],
-      city: [''],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.minLength(6), Validators.required]],
-      bankWeb: [''],
-      bankTelephone: [''],
-      bankFax: [''],
-      isBank: ['', Validators.required]
-    });
-  }
-
-  get f() { return this.registerNewBankForm.controls; }
-
-  onSubmit() {
-    this.submitted = true;
-    this.loading = true;
-
-  }
-
-}
->>>>>>> bb6ec7907fbcfe41ee3abae10c064bbd72b4418d
